a6d286a2f7f8c1ff113bc12424500bb06e80ad46
mateus_pereira2018 <mpereira@noovi.com.br>
2024-07-08 08:47:19 -0300

    Projeto criado

---
+++ backend/loja-virtual/src/test/java/com/msp/loja_virtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msp.loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
